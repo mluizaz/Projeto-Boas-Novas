9a8b5bcbe6fdf983733fc3b446408b17f0e3aa51
PVeeeeeee <pvitorss2004@gmail.com>
2023-11-10 03:01:54 -0300

    php e banco de dandos

--- process_form.php
+++ process_form.php
@@ -8,43 +8,50 @@ if (!$db) {
     die("Erro na conexão com o banco de dados");
 }
 
-$query = "CREATE TABLE IF NOT EXISTS contatos (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR,
-  email VARCHAR,
-  subject VARCHAR,
-  message TEXT
+$query = "CREATE TABLE IF NOT EXISTS avaliacoes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR,
+    email VARCHAR,
+    objetivo TEXT,
+    funcionalidade TEXT,
+    visual TEXT
 )";
 
 $result = $db->exec($query);
 
 if ($result !== false) {
-    echo "Tabela 'contatos' criada ou já existente.";
+    echo "Tabela 'avaliacoes' criada ou já existente. ";
 } else {
     die("Erro na criação da tabela: " . $db->lastErrorMsg());
 }
 
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') { // O restante do código relacionado ao formulário
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    $objetivo = $_POST['objetivo'];
+    $funcionalidade = $_POST['funcionalidade'];
+    $visual = $_POST['visual'];
 
-    $query = "INSERT INTO contatos (name, email, subject, message) VALUES (:name, :email, :subject, :message)";
+    $query = "INSERT INTO avaliacoes (name, email, objetivo, funcionalidade, visual) VALUES (:name, :email, :objetivo, :funcionalidade, :visual)";
     $stmt = $db->prepare($query);
     $stmt->bindParam(':name', $name, SQLITE3_TEXT);
     $stmt->bindParam(':email', $email, SQLITE3_TEXT);
-    $stmt->bindParam(':subject', $subject, SQLITE3_TEXT);
-    $stmt->bindParam(':message', $message, SQLITE3_TEXT);
+    $stmt->bindParam(':objetivo', $objetivo, SQLITE3_TEXT);
+    $stmt->bindParam(':funcionalidade', $funcionalidade, SQLITE3_TEXT);
+    $stmt->bindParam(':visual', $visual, SQLITE3_TEXT);
 
     $result = $stmt->execute();
 
-    if ($result !== false) {
-        echo "Dados do formulário foram armazenados com sucesso!";
-    } else {
-        echo "Erro ao armazenar dados do formulário: " . $db->lastErrorMsg();
-    }
+if ($result !== false) {
+    echo "Dados do formulário foram armazenados com sucesso!";
+} else {
+    echo "Erro ao armazenar dados do formulário.";
 }
-
+        
+}
+}
+        
 $db->close();
 ?>
